121f079bf971f8bc87c287e463059ad6ddb24ad9
MartinoVolcy <105470773+MartinoVolcy@users.noreply.github.com>
2022-08-21 13:05:32 -0400

    Update and rename Chap 14 - Using Web Services to Chap 14 - Using Web Services.py

--- Chap 14 - Using Web Services.py
+++ Chap 14 - Using Web Services.py
@@ -3,7 +3,7 @@
 #Extracting Data from XML
 
 #In this assignment you will write a Python program somewhat similar to http://www.py4e.com/code3/geoxml.py. 
-The program will prompt for a URL, read the XML data from that URL using urllib and then parse and extract the comment counts from the XML data, 
+#The program will prompt for a URL, read the XML data from that URL using urllib and then parse and extract the comment counts from the XML data, 
 #compute the sum of the numbers in the file.
 
 import urllib.request, urllib.parse, urllib.error
